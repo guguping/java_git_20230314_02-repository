f39dfaf0ceb1f07ce28eed36c99fe635666a7f65
guguping <qkfhandur@naver.com>
2023-03-14 13:06:50 +0900

    단추가

--- src/ex/Example2.java
+++ src/ex/Example2.java
@@ -5,6 +5,7 @@ public class Example2 {
 	public void numberTable() {
 		System.out.println("구구단 출력");
 		for (int i = 2; i <= 9; i++) {
+			System.out.println(i+"단");
 			for (int a = 1; a <= 9; a++) {
 				System.out.println(i + "*" + a + "=" + (a * i));
 			}
